14013de734a030fb0e99ca68e4032cfb2d155117
brendanmilton <brendan.milton@metrics.com.au>
2023-12-15 21:22:28 +1100

    Commiting initial skeleton project including package install, mock data, Nav and Side bars.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./pages/global/Navbar";
+// import Sidebar from "./pages/global/Sidebar";
+// import Dashboard from "./pages/dashboard";
+// import Pipeline from "./pages/pipeline"
+// import Team from "./pages/team";
+// import Quotes from "./pages/qoutes"
+// import Invoices from "./pages/invoices";
+// import Contacts from "./pages/contacts";
+// import FAQ from "./pages/faq";
+
+// import Bar from "./pages/bar";
+// import Form from "./pages/form";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
+// import Geography from "./pages/geography";
+// import Calendar from "./pages/calendar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [theme, colorMode] = useMode();
+  
+  return ( 
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/pipeline" element={<Pipeline />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/quotes" element={<Quotes />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Pie />} />
+              <Route path="/geography" element={<Pie />} />
+              <Route path="/calendar" element={<Pie />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
 export default App;
